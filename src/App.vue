ef4faa4661f17ac0c81dc858715324c77a049fcd
paulozemr <pauloramosj213@gmail.com>
2022-04-27 09:26:20 -0300

    Começo do site , feito os inputs , css e inicio de um componente

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script>
+import CalculosHipotenusa from "@/components/CalculosHipotenusa.vue";
+export default {
+  components: { CalculosHipotenusa },
+};
+</script>
+<template>
+  <header id="header">HipooTenusa!</header>
+  <CalculosHipotenusa />
+  <main id="content">Desenho do triangulo-retangulo</main>
+  <footer id="footer"></footer>
+</template>
---
+++ src/components/CalculosHipotenusa.vue
@@ -0,0 +1,42 @@
+<script></script>
+<template>
+  <aside>
+    <input id="Adja" type="number" placeholder="Adjacente" />
+    <input id="Oposto" type="number" placeholder="Oposto" />
+    <button id="calcular" placeholder="Calcular"></button>
+  </aside>
+</template>
+<style>
+aside {
+  background: #da4453; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #89216b,
+    #da4453
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #89216b,
+    #da4453
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+
+#Adja {
+  padding-left: 10px;
+  margin: 10px;
+  border-radius: 1px;
+  border-color: blue;
+}
+#Oposto {
+  padding-left: 10px;
+  margin: 10px;
+  border-radius: 2px;
+  border-color: red;
+}
+#calcular {
+  padding-left: 10px;
+  height: 25px;
+  width: 25px;
+  margin: 10px;
+}
+</style>
